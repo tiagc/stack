2789a3e09d7a1b9c5364ce6edf4b8b5018067cec
Tiago Canha <canha.tiago@muffag.ch>
2025-07-09 14:47:01 +0200

    stack icon and color config for stacks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">Stack</header>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        stackYellow: "#EEF41A",
+        stackGreen: "#22CE6D",
+        stackOrange: "#FE8339",
+        stackLightGray: "#B1DDDC",
+        stackBlue: "#7187FF",
+        stackPink: "#FE99C9",
+        stackRed: "#FD5D5C",
+        stackBabyBlue: "#9DCCFC",
+        stackMint: "#2CD2A7",
+        stackGray: "#8B8BAE",
+      },
+      fontFamily: {
+        neue: ['"Helvetica Neue"', "Arial", "sans-serif"],
+        geist: ['"Geist Mono"', "monospace"],
+      },
+    },
   },
   plugins: [],
 };
