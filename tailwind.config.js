9a7a588dcfcc592119f1479355e238b3230f69d2
Tiago Canha <canha.tiago@muffag.ch>
2025-07-09 14:23:32 +0200

    fixed error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
