b914fdaf51ccfebc7a3693616f55051da73ec160
Tiago Canha <canha.tiago@muffag.ch>
2025-07-17 15:29:49 +0200

    styles

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -16,9 +16,9 @@ export function Stack(props: {
     <ul className="relative">
       <li
         onClick={props.onToggle}
-        className={`text-black dark:text-white rounded-3xl mx-4 px-4 mb-4 py-6 border border-black dark:border-white relative overflow-hidden cursor-pointer ${
-          props.isExpanded ? "pb-auto" : ""
-        }`}>
+        className={`text-black dark:text-white rounded-3xl mx-4 px-4 mb-4 py-6 relative overflow-hidden cursor-pointer ${
+          props.color
+        } ${props.isExpanded ? "pb-auto" : ""}`}>
         {/* stack header */}
         <div className="flex items-center justify-between w-full">
           <span className="font-bold text-2xl">{props.label}</span>
@@ -42,14 +42,14 @@ export function Stack(props: {
               animate={{ opacity: 1, height: "auto" }}
               exit={{ opacity: 0, height: 0 }}
               transition={{ duration: 0.2, ease: "easeInOut" }}>
-              <div className="flex justify-between items-center mb-2">
+              <div className="flex justify-between items-center">
                 <span className="text-sm">Progress</span>
-                <span className="text-sm">
+                <span className="text-sm pr-1">
                   {props.currentProgress} / {props.goalPerDay}
                 </span>
               </div>
 
-              <div className="text-xs">
+              <div className="text-sm">
                 <span className="text-gray-400">Days: </span>
                 {props.days.length > 0 ? props.days.join(", ") : "None"}
               </div>
